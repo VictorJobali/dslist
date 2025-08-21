3282a6f6f688307c787f78c9f1df631ca6aba6c6
João Victor Batista de Oliveira <jvboliveira0405@gmail.com>
2025-08-20 22:00:41 -0300

    GET lists

---
+++ src/main/java/com/victor/dslist/repositories/GameListRepository.java
@@ -0,0 +1,11 @@
+package com.victor.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victor.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+	
+	
+}
